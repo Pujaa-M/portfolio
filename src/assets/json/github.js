d6bf13ebc8d971dafd963eb557ca8e492bef041d
Pujaa M <pujaa.m@ei.study>
2024-07-18 01:50:03 +0530

    UI update

--- src/assets/json/github.js
+++ src/assets/json/github.js
@@ -1,5 +1,6 @@
 import PinterestThumbnail from '../images/pinterest-clone-thumbnail.png'
 import ShoppingAppThumbnail from '../images/shopping-app-thumbnail.png'
+import DigitalMarketingThumbnail from '../images/digital-marketing-thumbnail.PNG'
 
 export const GithubProjects = [
     {
@@ -13,5 +14,11 @@ export const GithubProjects = [
         url: 'https://github.com/Pujaa-M/shopping-app',
         thumbnail: ShoppingAppThumbnail,
         desc: 'A simple shopping app that I developed using Angular in which basic operations such as adding to cart, removing from cart, calculating total cost, etc., are implemented using NgRX state management.'
+    },
+    {
+        link: 'digital-marketing-agency',
+        url: 'https://github.com/Pujaa-M/digital-marketing-agency',
+        thumbnail: DigitalMarketingThumbnail,
+        desc: 'Developed a homepage from the provided Adobe XD UI/UX Design for the Fyle Internship'
     }
 ]
--- src/components/shared/card-component/CardComponent.js
+++ src/components/shared/card-component/CardComponent.js
@@ -5,17 +5,17 @@ const CardComponent =({thumbnail, link, url, desc}) => {
         <div className="card-display">
             <img className="card-image" src={thumbnail} alt="thumbnail"></img>
 
-            <a href={url} target="_blank" rel="noreferrer"><div className="card-link">
-                <p className="link">{link}</p>
-                <img className="arrow-up-right" src={ArrowUpRight} alt="arrow-to-link"></img>
-            </div></a>
-
             <div className="tint"></div>
         </div>
         
         <div className="card-desc">
             <p>{desc}</p>
         </div>
+
+        <a href={url} target="_blank" rel="noreferrer"><div className="card-link">
+                <p className="link">{link}</p>
+                <img className="arrow-up-right" src={ArrowUpRight} alt="arrow-to-link"></img>
+            </div></a>
     </div>
 }
 
